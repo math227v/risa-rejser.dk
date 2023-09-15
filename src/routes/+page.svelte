2359e6fb9161b6e1cf79e0bda3586e069b683c3b
Mathias Lauridsen <math227v@gmail.com>
2023-09-15 20:47:34 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Timeslot from "../lib/timeslot.svelte";
+</script>
+
+<Timeslot title="Mandag"      startTime="2023-09-11T08:00:00" endTime="2023-09-11T15:00:00" />
+<Timeslot title="Tirsdag"     startTime="2023-09-12T08:00:00" endTime="2023-09-12T12:45:00" />
+<Timeslot title="Onsdag"      startTime="2023-09-13T08:00:00" endTime="2023-09-13T13:55:00" />
+<Timeslot title="Torsdag"     startTime="2023-09-14T08:00:00" endTime="2023-09-14T15:00:00" />
+<Timeslot title="Fredag"      startTime="2023-09-15T08:00:00" endTime="2023-09-15T15:00:00" />
+<br>
+<Timeslot title="Total uge"   startTime="2023-09-11T08:00:00" endTime="2023-09-15T15:00:00" />
+<!-- <Timeslot title="Fredag"      startTime="2023-08-11T08:00:00" endTime="2023-08-11T15:00:00" /> -->
