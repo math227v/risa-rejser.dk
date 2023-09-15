2e7dec7d382f07dda1f3840f10a01e9d91a4c0a4
Mathias Lauridsen <math227v@gmail.com>
2023-09-15 21:02:28 +0200

    inital push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,1 @@
-<script>
-    import Timeslot from "../lib/timeslot.svelte";
-</script>
-
-<Timeslot title="Mandag"      startTime="2023-09-11T08:00:00" endTime="2023-09-11T15:00:00" />
-<Timeslot title="Tirsdag"     startTime="2023-09-12T08:00:00" endTime="2023-09-12T12:45:00" />
-<Timeslot title="Onsdag"      startTime="2023-09-13T08:00:00" endTime="2023-09-13T13:55:00" />
-<Timeslot title="Torsdag"     startTime="2023-09-14T08:00:00" endTime="2023-09-14T15:00:00" />
-<Timeslot title="Fredag"      startTime="2023-09-15T08:00:00" endTime="2023-09-15T15:00:00" />
-<br>
-<Timeslot title="Total uge"   startTime="2023-09-11T08:00:00" endTime="2023-09-15T15:00:00" />
-<!-- <Timeslot title="Fredag"      startTime="2023-08-11T08:00:00" endTime="2023-08-11T15:00:00" /> -->
+<h1 class="text-5xl">Risa Rejser</h1>
