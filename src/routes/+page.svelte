3965b1d2f7c629a7b73376982a329bf5108c99a4
Mathias Lauridsen <math227v@gmail.com>
2023-09-15 23:11:23 +0200

    New colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,42 @@
-<div class="bg-green-900 text-white">
-    <h2 class="text-center font-bold uppercase bg-yellow-500 text-black shadow-xl p-4 mb-4 text-xl">⚠ Fortroligt ⚠</h2>
-    <div class="px-8 pb-8">
-        <h1 class="text-2xl font-bold text-center">INDKALDELSE TIL Q-ALLIANCEN</h1>
-        <p class="my-5">
-        Situationen i sektoren er kritisk. Armus`Hadar vinder frem med hidtil uset hastighed. Dette sætter os under et stort pres. Derfor bliver du indkaldt til Q-Alliancens træningslejr på planeten Risa. 
-        I træningslejren vil du indgå i Q-Alliancens indsatsstyrke, og deltage i træningen.</p>
+<div class="bg-milgreen text-white min-h-screen flex flex-col">
+    
+    <h2 class="text-center font-bold uppercase bg-milyellow text-black shadow-xl p-4 mb-8 text-xl">⚠ Fortroligt ⚠</h2>
 
-        <p class="my-5"><span class="font-bold">BEMÆRK!</span> Dette er en yderst risikabel mission, klassificeret fortrolig. Det betyder at af hensyn til sikkerheden, er det vigtigt at INGEN andre finder ud af noget om denne mission. Dette ville kunne forvolde alvorlig skade på Q-Alliancen, og ethvert brud på denne tavshedspligt vil blive straffet. 
-        Stol ikke på nogen. Selv Q-Alliancens eksistens skal holdes hemmelig.</p>
+    <div class="justify-center self-center m-auto h-full">
+        <div class="px-8 pb-8 max-w-xl">
+            <h1 class="text-2xl font-bold text-center pb-8">INDKALDELSE TIL Q-ALLIANCEN</h1>
+            <p>Kære rekrut.</p>
+            <p>Jeg er glad for at du så igennem den falske ferierejse, og nu har fundet din indkaldelse til Q-Alliancens
+                indsatsstyrke.</p>
 
-        <p class="my-5">Alle nye rekrutter i træningslejren bedes medbringe følgende:</p>
-        <ul>
-            <li>En rulle skraldeposer </li>
-            <li>Håndklæde</li>
-            <li>Skriveredskaber</li> 
-        </ul>
+            <p class="my-5">Situationen i vores sektor er kritisk. Armus`Hadar vinder frem med hidtil uset hastighed. Dette
+                sætter os under et stort pres. Derfor bliver du indkaldt til Q-Alliancens træningslejr på planeten Risa.
+                I træningslejren vil du indgå i Q-Alliancens indsatsstyrke, og deltage i træningen.</p>
 
-        <p class="my-5">
-            Andreas, Seniorsergent<br>
-            Matilde, Seniorsergent<br>
-            Mathias, Øverstkommanderende
-        </p>
+            <p class="my-5"><span class="font-bold">BEMÆRK!</span> Dette er en yderst risikabel mission, klassificeret
+                fortrolig. Det betyder at af hensyn til sikkerheden, er det vigtigt at INGEN andre finder ud af noget om
+                denne mission. Dette ville kunne forvolde alvorlig skade på Q-Alliancen, og ethvert brud på denne
+                tavshedspligt vil blive straffet. </p>
+            <p>Stol ikke på nogen. Selv Q-Alliancens eksistens skal holdes hemmelig.</p>
 
-    <h2>Q-Alliancen</h2>
+            <p class="my-5">Alle nye rekrutter i træningslejren bedes medbringe følgende:</p>
+            <ul class="list-disc ml-4">
+                <li>En rulle skraldeposer </li>
+                <li>Håndklæde</li>
+                <li>Skriveredskaber</li>
+            </ul>
+
+            <p class="my-5">Har du spørgsmål til din udsendelse, kan du altid tage kontakt til Mathias på SMS 42 73 73 92.</p>
+
+            <p class="mb-2 mt-5">Held og lykke rekrut,</p>
+
+            <p class="">
+                Andreas,<br>
+                Matilde,<br>
+                Mathias
+            </p>
+
+            <h2 class="text-xl font-bold my-5">Q-Alliancen</h2>
+        </div>
     </div>
 </div>
-
-<!-- This is a test -->
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      ibm: ['IBM Plex Sans', 'sans-serif']
+      ibm: ['IBM Plex Sans', 'sans-serif'],
+      colors: {
+        'milgreen': '#575b28',
+        'milyellow': '#c8b372'
+      }
     },
   },
   plugins: [],
