13d712617c16853564270422403a4afa278a024d
Mathias Lauridsen <math227v@gmail.com>
2023-09-16 11:33:50 +0200

    Infoboks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,41 +2,53 @@
     
     <h2 class="text-center font-bold uppercase bg-milyellow text-black shadow-xl p-4 mb-8 text-xl">⚠ Fortroligt ⚠</h2>
 
-    <div class="justify-center self-center m-auto h-full">
-        <div class="px-8 pb-8 max-w-xl">
-            <h1 class="text-2xl font-bold text-center pb-8">INDKALDELSE TIL Q-ALLIANCEN</h1>
-            <p>Kære rekrut.</p>
-            <p>Jeg er glad for at du så igennem den falske ferierejse, og nu har fundet din indkaldelse til Q-Alliancens
-                indsatsstyrke.</p>
+    <div class="flex flex-col-reverse lg:flex-row m-auto">
+        <div class="justify-center self-center h-full">
+            <div class="px-8 pb-8 max-w-xl">
+                <h1 class="text-2xl font-bold text-center pb-8">INDKALDELSE TIL Q-ALLIANCEN</h1>
+                <p>Kære rekrut.</p>
+                <p>Jeg er glad for at du så igennem den falske ferierejse, og nu har fundet din indkaldelse til Q-Alliancens
+                    indsatsstyrke.</p>
 
-            <p class="my-5">Situationen i vores sektor er kritisk. Armus`Hadar vinder frem med hidtil uset hastighed. Dette
-                sætter os under et stort pres. Derfor bliver du indkaldt til Q-Alliancens træningslejr på planeten Risa.
-                I træningslejren vil du indgå i Q-Alliancens indsatsstyrke, og deltage i træningen.</p>
+                <p class="my-5">Situationen i vores sektor er kritisk. Armus`Hadar vinder frem med hidtil uset hastighed. Dette
+                    sætter os under et stort pres. Derfor bliver du indkaldt til Q-Alliancens træningslejr på planeten Risa.
+                    I træningslejren vil du indgå i Q-Alliancens indsatsstyrke, og deltage i træningen.</p>
 
-            <p class="my-5"><span class="font-bold">BEMÆRK!</span> Dette er en yderst risikabel mission, klassificeret
-                fortrolig. Det betyder at af hensyn til sikkerheden, er det vigtigt at INGEN andre finder ud af noget om
-                denne mission. Dette ville kunne forvolde alvorlig skade på Q-Alliancen, og ethvert brud på denne
-                tavshedspligt vil blive straffet. </p>
-            <p>Stol ikke på nogen. Selv Q-Alliancens eksistens skal holdes hemmeligt overfor alle og enhver.</p>
+                <p class="my-5"><span class="font-bold">BEMÆRK!</span> Dette er en yderst risikabel mission, klassificeret
+                    fortrolig. Det betyder at af hensyn til sikkerheden, er det vigtigt at INGEN andre finder ud af noget om
+                    denne mission. Dette ville kunne forvolde alvorlig skade på Q-Alliancen, og ethvert brud på denne
+                    tavshedspligt vil blive straffet. </p>
+                <p>Stol ikke på nogen. Selv Q-Alliancens eksistens skal holdes hemmeligt overfor alle og enhver.</p>
 
-            <p class="my-5">Alle nye rekrutter i træningslejren bedes medbringe følgende:</p>
-            <ul class="list-disc ml-4">
-                <li>En rulle skraldeposer </li>
-                <li>Håndklæde</li>
-                <li>Skriveredskaber</li>
-            </ul>
+                <p class="my-5">Alle nye rekrutter i træningslejren bedes medbringe følgende:</p>
+                <ul class="list-disc ml-4">
+                    <li>En rulle skraldeposer </li>
+                    <li>Håndklæde</li>
+                    <li>Skriveredskaber</li>
+                    <li>Mikroovnspopcorn</li>
+                    <li>Dagstursrygsæk</li>
+                </ul>
 
-            <p class="my-5">Har du spørgsmål til din udsendelse, kan du altid tage kontakt til Mathias på SMS 42 73 73 92.</p>
+                <p class="my-5">Har du spørgsmål til din udsendelse, kan du altid tage kontakt til Mathias på SMS 42 73 73 92.</p>
 
-            <p class="mb-2 mt-5">Held og lykke rekrut,</p>
+                <p class="mb-2 mt-5">Held og lykke rekrut,</p>
 
-            <p class="">
-                Andreas,<br>
-                Matilde,<br>
-                Mathias
-            </p>
+                <p class="">
+                    A. Strøm, M. Nielsen, & M. Lauridsen
+                </p>
 
-            <h2 class="text-xl font-bold my-5">Q-Alliancen</h2>
+                <h2 class="text-xl font-bold my-5">Q-Alliancen</h2>
+            </div>
+        </div>
+
+        <div class="font-mono mt-0 mb-8 lg:my-16 border shadow h-full p-4 mx-4">
+            <p class="font-bold text-center mb-4">Q-ALLIANCEN</p>
+            <pre class="">
+<span class="font-bold">DATO       </span> 2023-10-14T15:30:00A
+<span class="font-bold">           </span> 2023-10-20T11:00:00A
+<span class="font-bold">STED       </span> <span class="bg-black text-white p-1 font-bold">            </span>
+<span class="font-bold">MISSIONSID </span> MSE<span class="bg-black text-white p-1 font-bold">       </span>SVK
+<span class="font-bold">DÆKNAVN    </span> Risa-Rejser</pre>
         </div>
     </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
   theme: {
     extend: {
       ibm: ['IBM Plex Sans', 'sans-serif'],
+      mono: ['IBM Plex Mono', 'monospace'],
       colors: {
         'milgreen': '#575b28',
         'milyellow': '#c8b372'
